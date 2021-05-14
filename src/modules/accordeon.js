6c9246d32cc90476d0331d847c6ea045453f7722
Kiskina Viktoriya <kiskina.vss@yandex.ru>
2021-05-14 18:31:48 +0300

    Accordeon, feedback

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,5 +1,30 @@
 const accordeon = () => {
+  const accordeon = document.querySelector('.accordeon'),
+        accordeonElem = accordeon.querySelectorAll('.element'),
+        accordeonTitle = accordeon.querySelectorAll('.title'),
+        accordeonContent = accordeon.querySelectorAll('.element-content');
+        console.log(accordeonContent);
+  accordeonElem[0].classList.add('active');
+  accordeonContent[0].classList.add('active');
+
+  const deleteElem = () => {
+    accordeonElem.forEach((item) => {
+      item.classList.remove('active');
+    });
+    accordeonContent.forEach((item, i) => {
+      item.classList.remove('active');
+    });
+  };
+
+  for (let i = 0; i < accordeonElem.length; i++) {
+    accordeonTitle[i].addEventListener('click', event => {
+      deleteElem();
+      accordeonElem[i].classList.add('active');
+      accordeonContent[i].classList.add('active');
+    });
+  }
 
 };
 
+accordeon();
 // export default accordeon;
